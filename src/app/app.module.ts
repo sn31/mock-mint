70bf4e67805170e0c5a9d30dc9422d0674037e72
SkyeN <skye@dames.es>
2018-10-23 13:16:03 -0700

    add ngx-page-scroll and seperate main page content into a new component

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,15 +3,18 @@ import { NgModule } from '@angular/core';
 
 import { AppComponent } from './app.component';
 import { NavbarComponent } from './navbar/navbar.component';
-
+import {NgxPageScrollModule} from 'ngx-page-scroll';
+import { MainPageContentComponent } from './main-page-content/main-page-content.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    NavbarComponent
+    NavbarComponent,
+    MainPageContentComponent
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    NgxPageScrollModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/main-page-content/main-page-content.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-main-page-content',
+  templateUrl: './main-page-content.component.html',
+  styleUrls: ['./main-page-content.component.css']
+})
+export class MainPageContentComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
