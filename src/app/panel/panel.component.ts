e6ba5bd684a0bb0342aca29812e484b205f49e93
SkyeN <skye@dames.es>
2018-10-25 10:39:14 -0700

    make panel-component resuable by passing data down from the root component

--- src/app/panel/panel.component.ts
+++ src/app/panel/panel.component.ts
@@ -1,12 +1,12 @@
-import { Component, OnInit } from '@angular/core';
-
+import { Component, OnInit, Input } from '@angular/core';
+import {Panel} from '../models/panel-model';
 @Component({
   selector: 'app-panel',
   templateUrl: './panel.component.html',
   styleUrls: ['./panel.component.css']
 })
 export class PanelComponent implements OnInit {
-
+  @Input() childPanelList: Panel[];
   constructor() { }
 
   ngOnInit() {
