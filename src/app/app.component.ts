6240238eeacc9be411f27d60e68e618b79983c51
SkyeN <skye@dames.es>
2018-10-26 10:42:52 -0700

    add firebase authentication configs

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,26 @@
-import { Component } from '@angular/core';
+import { Component } from "@angular/core";
+import { AuthenticationService } from "./authentication.service";
 
 @Component({
-  selector: 'app-root',
-  templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  selector: "app-root",
+  templateUrl: "./app.component.html",
+  styleUrls: ["./app.component.css"],
+  providers: [AuthenticationService]
 })
 export class AppComponent {
-  title = 'Mint';
-  
- 
+  title = "Mint";
+  user;
+
+  constructor(public authService: AuthenticationService){
+    this.authService.user.subscribe(user=>{
+      console.log(user);
+    });
+  }
+
+  login() {
+    this.authService.login();
+  }
+  logout() {
+    this.authService.logout();
+  }
 }
