c75aba70d5525482cd9e842b78aa8e1bef4a26db
SkyeN <skye@dames.es>
2018-10-26 11:58:42 -0700

    successfully add google authentication service

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,26 +1,12 @@
 import { Component } from "@angular/core";
-import { AuthenticationService } from "./authentication.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
-  providers: [AuthenticationService] 
+ 
 })
 export class AppComponent {
   title = "Mint";
-  user;
-
-  constructor(public authService: AuthenticationService){
-    this.authService.user.subscribe(user=>{
-      console.log(user);
-    });
-  }
-
-  login() {
-    this.authService.login();
-  }
-  logout() {
-    this.authService.logout();
-  }
+  
 }
--- src/app/navbar/navbar.component.ts
+++ src/app/navbar/navbar.component.ts
@@ -1,9 +1,10 @@
 import { Component, OnInit } from '@angular/core';
-
+import { AuthenticationService } from "../authentication.service";
 @Component({
   selector: 'app-navbar',
   templateUrl: './navbar.component.html',
-  styleUrls: ['./navbar.component.css']
+  styleUrls: ['./navbar.component.css'],
+  providers: [AuthenticationService] 
 })
 export class NavbarComponent implements OnInit {
   navbarOpen = false;
@@ -12,7 +13,28 @@ export class NavbarComponent implements OnInit {
   {
     this.navbarOpen = !this.navbarOpen;
   }
-  constructor() { }
+  user;
+  private isLoggedIn: Boolean;
+  private userName: String;
+
+  constructor(public authService: AuthenticationService){
+    this.authService.user.subscribe(user=>{
+      if (user === null){
+        this.isLoggedIn = false;
+      }
+      else {
+        this.isLoggedIn = true;
+        this.userName = user.displayName;
+      }
+    });
+  }
+
+  login() {
+    this.authService.login();
+  }
+  logout() {
+    this.authService.logout();
+  }
 
   ngOnInit() {
   }
