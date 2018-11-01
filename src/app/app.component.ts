d5597ce8a4c610cb51af50f3ada42600ff52b214
SkyeN <skye@dames.es>
2018-11-01 13:07:00 -0700

    Remove navbar component from homepage component. Remove privatenav component from private component. Inject authentication service to app component to only show nav if the user is logged in.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,12 +1,24 @@
 import { Component } from "@angular/core";
+import { AuthenticationService } from "./authentication.service";
 
 @Component({
   selector: "app-root",
   templateUrl: "./app.component.html",
   styleUrls: ["./app.component.css"],
- 
+  providers: [AuthenticationService] 
 })
 export class AppComponent {
   title = "Mint";
-  
+  user;
+  private isLoggedIn: Boolean;
+
+  constructor(public authService: AuthenticationService){
+    this.authService.user.subscribe(user=>{
+      if (user === null)
+      {
+        this.isLoggedIn = true;
+      }
+      else {this.isLoggedIn = false};
+    })
+  }
 }
