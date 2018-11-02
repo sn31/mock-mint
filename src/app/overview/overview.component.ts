04396763a22f2eb88d17d09dc75008be91c3ee57
SkyeN <skye@dames.es>
2018-11-02 13:48:54 -0700

    add chart.js to overview page --not showing data

--- src/app/overview/overview.component.ts
+++ src/app/overview/overview.component.ts
@@ -1,17 +1,37 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit,AfterViewInit  } from '@angular/core';
 import {Account} from '../models/account-model';
+import { TransactionService } from '../services/transaction.service';
+import { FirebaseListObservable } from 'angularfire2/database';
+import { Chart } from 'chart.js';
+
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
-  styleUrls: ['./overview.component.css']
+  styleUrls: ['./overview.component.css'],
+  providers: [TransactionService]
 })
-export class OverviewComponent implements OnInit {
+export class OverviewComponent implements OnInit, AfterViewInit{
+  chart =[];
+  transactions: FirebaseListObservable<any[]>;
+  amountArr: number[] = [];
 
-  constructor() { }
-
+  constructor( private transactionService: TransactionService) {}
   ngOnInit() {
-   
+    this.transactions= this.transactionService.getTransactions();
+    this.transactions.forEach(element => {
+      element.forEach(subElement=>{
+        this.amountArr.push(subElement["Amount"]);
+      })
+    });
   }
+
+  ngAfterViewInit(){
+    this.chart = (new Chart('canvas', {
+      type: 'line',
+      data: [1,2,4,61,1,2,4,56,2,13,2,1,2]
+    }));
+  }
+
   currentTime = new Date();
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
@@ -20,4 +40,5 @@ export class OverviewComponent implements OnInit {
   newAccount = new Account(
     '100000','10000','-40000','135000','35000'
   )
+  
 }
