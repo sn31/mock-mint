09d2e202d719038506f6899b793a95fc05bd2b9c
SkyeN <skye@dames.es>
2018-11-02 08:42:42 -0700

    add routing for overview component

--- src/app/app.routing.ts
+++ src/app/app.routing.ts
@@ -3,6 +3,7 @@ import { Routes, RouterModule } from '@angular/router';
 import {HomepageComponent} from '../app/homepage/homepage.component';
 import {PrivateComponent} from '../app/private/private.component';
 import { TransactionComponent } from '../app/transaction/transaction.component';
+import {OverviewComponent} from '../app/overview/overview.component';
 const appRoutes: Routes = [
     {
       path: '',
@@ -16,7 +17,10 @@ const appRoutes: Routes = [
     {
       path:'private/transactions',
       component: TransactionComponent
-    }
-
+    },
+    {
+      path:'private/overview',
+      component: OverviewComponent
+    },
   ];
 export const routing: ModuleWithProviders = RouterModule.forRoot(appRoutes);
